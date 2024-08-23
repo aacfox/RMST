9550a5da9ea30a3e3f7ce2d79f19a3e423d94f21
aacfox <aacfox@users.noreply.github.com>
2024-08-23 19:48:28 +0000

    deploy: 5b7f95573d1dc558cd12d43cd94052cad434513e

--- search/all_b.js
+++ search/all_b.js
@@ -6,6 +6,7 @@ var searchData=
   ['max_5fv_3',['max_v',['../namespaceboil_1_1utilities.html#a65c959a85f6327c153814441e08c2756',1,'boil::max_v'],['../namespaceboil_1_1utilities.html#a65c959a85f6327c153814441e08c2756',1,'boil::utilities::max_v']]],
   ['mermaid_4',['mermaid',['../classGraph.html#a13c4fe6e7b3153ee2fa2a6d79838edfa',1,'Graph']]],
   ['min_5fv_5',['min_v',['../namespaceboil_1_1utilities.html#af4fbd541ada4a7eecf3b675d77457d2c',1,'boil::min_v'],['../namespaceboil_1_1utilities.html#af4fbd541ada4a7eecf3b675d77457d2c',1,'boil::utilities::min_v']]],
-  ['mst_20algo_6',['Randomized O(m) MST algo',['../index.html#autotoc_md12',1,'']]],
-  ['mst_20algos_7',['Other cool MST algos',['../index.html#autotoc_md13',1,'']]]
+  ['mst_6',['mst',['../classGraph.html#a3601c6a236196c427f9fc475eacf57bb',1,'Graph']]],
+  ['mst_20algo_7',['Randomized O(m) MST algo',['../index.html#autotoc_md12',1,'']]],
+  ['mst_20algos_8',['Other cool MST algos',['../index.html#autotoc_md13',1,'']]]
 ];
