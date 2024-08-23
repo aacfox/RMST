dd22471f5095d9005e05ea4e078c01eb82940918
aacfox <aacfox@users.noreply.github.com>
2024-08-23 19:34:49 +0000

    deploy: ae9567e0f0031961053723be1ba4e03bda32830e

--- search/all_10.js
+++ search/all_10.js
@@ -4,5 +4,6 @@ var searchData=
   ['test_1',['Boruvka&apos;s subroutine test',['../md_docs_2CONTRIBUTING.html#autotoc_md3',1,'']]],
   ['test_20cases_2',['Test cases',['../md_docs_2CONTRIBUTING.html#autotoc_md1',1,'']]],
   ['the_20algorithm_20s_20source_20code_3',['The algorithm&apos;s source code',['../index.html#autotoc_md9',1,'']]],
-  ['today_4',['today',['../namespaceboil_1_1utilities.html#af3feae0c9cbba27fea45a0e9d363b589',1,'boil::today()'],['../namespaceboil_1_1utilities.html#af3feae0c9cbba27fea45a0e9d363b589',1,'boil::utilities::today()']]]
+  ['the_20subroutines_20of_20boruvka_4',['one of the subroutines of boruvka()',['..//github/workspace/graph.cpp#trim',1,'']]],
+  ['today_5',['today',['../namespaceboil_1_1utilities.html#af3feae0c9cbba27fea45a0e9d363b589',1,'boil::today()'],['../namespaceboil_1_1utilities.html#af3feae0c9cbba27fea45a0e9d363b589',1,'boil::utilities::today()']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -2,12 +2,14 @@ var searchData=
 [
   ['o_20m_20mst_20algo_0',['Randomized O(m) MST algo',['../index.html#autotoc_md12',1,'']]],
   ['o_5fclock_1',['o_clock',['../namespaceboil_1_1utilities.html#a993d135bb2169e9db5278880d9d7e7d5',1,'boil::o_clock()'],['../namespaceboil_1_1utilities.html#a993d135bb2169e9db5278880d9d7e7d5',1,'boil::utilities::o_clock()']]],
-  ['ok_2',['ok',['../classboil_1_1classes_1_1Combination.html#ac6cb6ddb175bbe3f9c7d7f2bfd19006d',1,'boil::classes::Combination']]],
-  ['operator_2a_3',['operator*',['../classboil_1_1classes_1_1Combination.html#a753b06e9e45784eaa42a61072a7e9e87',1,'boil::classes::Combination']]],
-  ['operator_2b_2b_4',['operator++',['../classboil_1_1classes_1_1Combination.html#abf9e8ffd2e3cacb04be0584eec66984b',1,'boil::classes::Combination']]],
-  ['operator_3c_3d_3e_5',['operator&lt;=&gt;',['../structGraph_1_1Edge.html#a2d2f39e86602ee2867789a3accadb419',1,'Graph::Edge']]],
-  ['operator_3d_3d_6',['operator==',['../structGraph_1_1Edge.html#a111a7651f4f6cbef8e04108c7ed58555',1,'Graph::Edge']]],
-  ['order_7',['order',['../namespaceboil_1_1utilities.html#a9a0f42ff93c5f580e2246ac5fa24617a',1,'boil::order'],['../namespaceboil_1_1utilities.html#a9a0f42ff93c5f580e2246ac5fa24617a',1,'boil::utilities::order']]],
-  ['origin_8',['origin',['../structGraph_1_1Edge.html#a2fec859a16640d6f48d781f362a9449e',1,'Graph::Edge']]],
-  ['other_20cool_20mst_20algos_9',['Other cool MST algos',['../index.html#autotoc_md13',1,'']]]
+  ['of_20the_20subroutines_20of_20boruvka_2',['one of the subroutines of boruvka()',['..//github/workspace/graph.cpp#trim',1,'']]],
+  ['ok_3',['ok',['../classboil_1_1classes_1_1Combination.html#ac6cb6ddb175bbe3f9c7d7f2bfd19006d',1,'boil::classes::Combination']]],
+  ['one_20of_20the_20subroutines_20of_20boruvka_4',['one of the subroutines of boruvka()',['..//github/workspace/graph.cpp#trim',1,'']]],
+  ['operator_2a_5',['operator*',['../classboil_1_1classes_1_1Combination.html#a753b06e9e45784eaa42a61072a7e9e87',1,'boil::classes::Combination']]],
+  ['operator_2b_2b_6',['operator++',['../classboil_1_1classes_1_1Combination.html#abf9e8ffd2e3cacb04be0584eec66984b',1,'boil::classes::Combination']]],
+  ['operator_3c_3d_3e_7',['operator&lt;=&gt;',['../structGraph_1_1Edge.html#a2d2f39e86602ee2867789a3accadb419',1,'Graph::Edge']]],
+  ['operator_3d_3d_8',['operator==',['../structGraph_1_1Edge.html#a111a7651f4f6cbef8e04108c7ed58555',1,'Graph::Edge']]],
+  ['order_9',['order',['../namespaceboil_1_1utilities.html#a9a0f42ff93c5f580e2246ac5fa24617a',1,'boil::order'],['../namespaceboil_1_1utilities.html#a9a0f42ff93c5f580e2246ac5fa24617a',1,'boil::utilities::order']]],
+  ['origin_10',['origin',['../structGraph_1_1Edge.html#a2fec859a16640d6f48d781f362a9449e',1,'Graph::Edge']]],
+  ['other_20cool_20mst_20algos_11',['Other cool MST algos',['../index.html#autotoc_md13',1,'']]]
 ];
